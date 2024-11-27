0fde480a6d78466772f4cad9a7d7e4032532df25
Saumya Phor <saumya.phor4252@gmail.com>
2024-11-27 11:13:15 +0100

    Update the configs for DeepDive 2024 testing

---
+++ DeepDive2024/README.md
@@ -0,0 +1,75 @@
+### Set up the rucio rules for the files/blocks as per required minimally for the testing
+```
+rucio add-rule cms:/EGamma0/Run2024G-ZElectron-PromptReco-v1/RAW-RECO#0c946de0-a120-4210-89a0-65fea93afc08 1 T2_CH_CERN --lifetime 259200 --comment "For urgent EGM Deep Dive studies"
+33aaefdb3e764fcd8faeed0b9f8d4a6e
+rucio add-rule cms:/EGamma0/Run2024G-ZElectron-PromptReco-v1/RAW-RECO#19000750-6ead-4130-b07c-9627f4a68171 1 T2_CH_CERN --lifetime 259200 --comment "For urgent EGM Deep Dive studies"
+3f9409d90498440282596633e8f6b284
+rucio add-rule cms:/EGamma0/Run2024G-ZElectron-PromptReco-v1/RAW-RECO#21c3f19c-fb78-45d2-a7fe-06204ad321a5 1 T2_CH_CERN --lifetime 259200 --comment "For urgent EGM Deep Dive studies"
+b3368500d2a644598203c8833970a4fb
+rucio add-rule cms:/EGamma0/Run2024G-ZElectron-PromptReco-v1/RAW-RECO#37fa4659-271f-4f1c-9453-db6e079e6e00 1 T2_CH_CERN --lifetime 259200 --comment "For urgent EGM Deep Dive studies" 
+af48d2749241445c9be72e6f450a2914
+rucio add-rule cms:/EGamma0/Run2024G-ZElectron-PromptReco-v1/RAW-RECO#49711f9d-decf-4860-8b3e-e6634978ae2b 1 T2_CH_CERN --lifetime 259200 --comment "For urgent EGM Deep Dive studies"
+5aa880378fff4a4193c1ad44c5bddee3
+```
+
+### CMSSW set-up
+```
+cmsrel CMSSW_14_0_15
+cd cmsrel CMSSW_14_0_15/src
+cmsenv
+```
+
+### Run the L1T-HLT step
+```
+cmsDriver.py  --conditions 140X_dataRun3_HLT_v3 --data --datatier RECO --era Run3 --eventcontent RECO --filein file:/eos/cms/store/data/Run2024G/EGamma0/RAW-RECO/ZElectron-PromptReco-v1/000/385/447/00000/f596385d-f448-4f54-baee-93b8bd21e8e2.root --fileout file:hltOutput_RECO.root --no_exec -n 200 --process MYHLT --python_filename hlt_ReRun_Config.py --scenario pp --step L1REPACK:Full,HLT:GRun
+```
+
+This will create the config file to be used. Update the config manually to change the required values by adding editing this config file or directly in cmsCondor submission script:
+
+```
+# Load the GRun menu
+from HLTrigger.Configuration.HLT_GRun_cff import *
+# Modify parameters
+process.hltL1sSingleEGor.L1SeedsLogicalExpression = cms.string('L1_SingleLooseIsoEG26er2p5 OR L1_SingleLooseIsoEG26er1p5 OR L1_SingleLooseIsoEG28er2p5 OR L1_SingleLooseIsoEG28er2p1 OR L1_SingleLooseIsoEG28er1p5 OR L1_SingleLooseIsoEG30er2p5 OR L1_SingleLooseIsoEG30er1p5 OR L1_SingleEG26er2p5 OR L1_SingleEG38er2p5 OR L1_SingleEG40er2p5 OR L1_SingleEG42er2p5 OR L1_SingleEG45er2p5 OR L1_SingleEG60 OR L1_SingleEG34er2p5 OR L1_SingleIsoEG24er2p1 OR L1_SingleIsoEG26er2p1 OR L1_SingleIsoEG28er2p1 OR L1_SingleIsoEG30er2p1 OR L1_SingleIsoEG32er2p1 OR L1_SingleIsoEG26er2p5 OR L1_SingleIsoEG28er2p5 OR L1_SingleIsoEG32er2p5 OR L1_SingleIsoEG34er2p5')
+# Original
+#process.hltL1sSingleEGor.L1SeedsLogicalExpression = cms.string('L1_SingleLooseIsoEG26er2p5 OR L1_SingleLooseIsoEG26er1p5 OR L1_SingleLooseIsoEG28er2p5 OR L1_SingleLooseIsoEG28er2p1 OR L1_SingleLooseIsoEG28er1p5 OR L1_SingleLooseIsoEG30er2p5 OR L1_SingleLooseIsoEG30er1p5 OR L1_SingleEG26er2p5 OR L1_SingleEG38er2p5 OR L1_SingleEG40er2p5 OR L1_SingleEG42er2p5 OR L1_SingleEG45er2p5 OR L1_SingleEG60 OR L1_SingleEG34er2p5 OR L1_SingleEG36er2p5 OR L1_SingleIsoEG24er2p1 OR L1_SingleIsoEG26er2p1 OR L1_SingleIsoEG28er2p1 OR L1_SingleIsoEG30er2p1 OR L1_SingleIsoEG32er2p1 OR L1_SingleIsoEG26er2p5 OR L1_SingleIsoEG28er2p5 OR L1_SingleIsoEG30er2p5 OR L1_SingleIsoEG32er2p5 OR L1_SingleIsoEG34er2p5')
+```
+
+Now run the configuration file for local testing
+```
+cmsRun hlt_ReRun_Config.py
+```
+ 
+### Run the PAT step to create miniAOD
+
+
+### Condor Submission Tools
+```
+##### Input setup #####
+
+# Use the dasFileQuery script to control the number of files/events in the dataset you want to run on
+# dasgoclient --query "file dataset=/EGamma0/Run2024G-ZElectron-PromptReco-v1/RAW-RECO | grep file.name,file.nevents" --limit 1425 | awk '{sum += $2} END {print sum}'
+python3 dasFileQuery.py
+# This will create the List_cff.py file with the list of input files to be used.
+voms-proxy-init --valid 100:00
+cp /tmp/x509up_u<999999> /afs/cern.ch/user/s/ssaumya/private/x509up_u<999999>
+
+##### For HLT step #####
+
+# Update the cmsCondor.py accordingly for input and output, and the change needed in hltConfiguration
+## L49-L56 for configuration modification, L57-L74 for input source, L77 for events, L80-81 and L129-132 for output file name
+python3 cmsCondor.py hlt_ReRun_Config.py /afs/cern.ch/work/s/ssaumya/private/Egamma/CMSSW_14_0_15/src/ /eos/cms/store/group/phys_egamma/ssaumya/DeepDive/HLTstep_RECO_RootFiles/ -n 9 -q tomorrow -p /afs/cern.ch/user/s/ssaumya/private/x509up_u<999999>
+# -n 10 --> 10 files per job, 145 jobs created in this case
+./sub_total.jobb
+
+##### For PAT step #####
+
+# Update the cmsCondor.py accordingly for input and output, and the change needed in hltConfiguration  
+## L49-L53 for configuration modification, L55-L72 for input source, L75 for events, L78-79 and L127-130 for output file name
+python3 cmsCondor.py makeMini_cfg.py /afs/cern.ch/work/s/ssaumya/private/Egamma/EGM_Bpix/CMSSW_14_0_15/src/ /eos/cms/store/group/phys_egamma/ssaumya/EGM_BPix_Fix/PATstep_MINIAOD_RootFiles/ -n 5 -q tomorrow -p /afs/cern.ch/user/s/ssaumya/private/x509up_u<999999>
+```
+
+### Make the ntuples
+
+
+### Run Iason's tool for plotting
---
+++ DeepDive2024/hlt_ReRun_Config.py
@@ -0,0 +1,121 @@
+# Auto generated configuration file
+# using: 
+# Revision: 1.19 
+# Source: /local/reps/CMSSW/CMSSW/Configuration/Applications/python/ConfigBuilder.py,v 
+# with command line options: --conditions 140X_dataRun3_HLT_v3 --data --datatier RECO --era Run3 --eventcontent RECO --filein file:/eos/cms/store/data/Run2024G/EGamma0/RAW-RECO/ZElectron-PromptReco-v1/000/385/447/00000/f596385d-f448-4f54-baee-93b8bd21e8e2.root --fileout file:hltOutput_RECO.root --no_exec -n 200 --process MYHLT --python_filename hlt_ReRun_Config.py --scenario pp --step L1REPACK:Full,HLT:GRun
+import FWCore.ParameterSet.Config as cms
+
+from Configuration.Eras.Era_Run3_cff import Run3
+
+process = cms.Process('MYHLT',Run3)
+
+# import of standard configurations
+process.load('Configuration.StandardSequences.Services_cff')
+process.load('SimGeneral.HepPDTESSource.pythiapdt_cfi')
+process.load('FWCore.MessageService.MessageLogger_cfi')
+process.load('Configuration.EventContent.EventContent_cff')
+process.load('Configuration.StandardSequences.GeometryRecoDB_cff')
+process.load('Configuration.StandardSequences.MagneticField_cff')
+process.load('Configuration.StandardSequences.SimL1EmulatorRepack_Full_cff')
+process.load('HLTrigger.Configuration.HLT_GRun_cff')
+process.load('Configuration.StandardSequences.EndOfProcess_cff')
+process.load('Configuration.StandardSequences.FrontierConditions_GlobalTag_cff')
+
+process.maxEvents = cms.untracked.PSet(
+    input = cms.untracked.int32(200),
+    output = cms.optional.untracked.allowed(cms.int32,cms.PSet)
+)
+
+# Load the GRun menu
+from HLTrigger.Configuration.HLT_GRun_cff import *
+# Modify parameters
+process.hltL1sSingleEGor.L1SeedsLogicalExpression = cms.string('L1_SingleLooseIsoEG26er2p5 OR L1_SingleLooseIsoEG26er1p5 OR L1_SingleLooseIsoEG28er2p5 OR L1_SingleLooseIsoEG28er2p1 OR L1_SingleLooseIsoEG28er1p5 OR L1_SingleLooseIsoEG30er2p5 OR L1_SingleLooseIsoEG30er1p5 OR L1_SingleEG26er2p5 OR L1_SingleEG38er2p5 OR L1_SingleEG40er2p5 OR L1_SingleEG42er2p5 OR L1_SingleEG45er2p5 OR L1_SingleEG60 OR L1_SingleEG34er2p5 OR L1_SingleIsoEG24er2p1 OR L1_SingleIsoEG26er2p1 OR L1_SingleIsoEG28er2p1 OR L1_SingleIsoEG30er2p1 OR L1_SingleIsoEG32er2p1 OR L1_SingleIsoEG26er2p5 OR L1_SingleIsoEG28er2p5 OR L1_SingleIsoEG32er2p5 OR L1_SingleIsoEG34er2p5')
+# Original 
+#process.hltL1sSingleEGor.L1SeedsLogicalExpression = cms.string('L1_SingleLooseIsoEG26er2p5 OR L1_SingleLooseIsoEG26er1p5 OR L1_SingleLooseIsoEG28er2p5 OR L1_SingleLooseIsoEG28er2p1 OR L1_SingleLooseIsoEG28er1p5 OR L1_SingleLooseIsoEG30er2p5 OR L1_SingleLooseIsoEG30er1p5 OR L1_SingleEG26er2p5 OR L1_SingleEG38er2p5 OR L1_SingleEG40er2p5 OR L1_SingleEG42er2p5 OR L1_SingleEG45er2p5 OR L1_SingleEG60 OR L1_SingleEG34er2p5 OR L1_SingleEG36er2p5 OR L1_SingleIsoEG24er2p1 OR L1_SingleIsoEG26er2p1 OR L1_SingleIsoEG28er2p1 OR L1_SingleIsoEG30er2p1 OR L1_SingleIsoEG32er2p1 OR L1_SingleIsoEG26er2p5 OR L1_SingleIsoEG28er2p5 OR L1_SingleIsoEG30er2p5 OR L1_SingleIsoEG32er2p5 OR L1_SingleIsoEG34er2p5')
+
+# Input source
+process.source = cms.Source("PoolSource",
+    fileNames = cms.untracked.vstring('file:/eos/cms/store/data/Run2024G/EGamma0/RAW-RECO/ZElectron-PromptReco-v1/000/385/447/00000/f596385d-f448-4f54-baee-93b8bd21e8e2.root'),
+    secondaryFileNames = cms.untracked.vstring()
+)
+
+process.options = cms.untracked.PSet(
+    IgnoreCompletely = cms.untracked.vstring(),
+    Rethrow = cms.untracked.vstring(),
+    TryToContinue = cms.untracked.vstring(),
+    accelerators = cms.untracked.vstring('*'),
+    allowUnscheduled = cms.obsolete.untracked.bool,
+    canDeleteEarly = cms.untracked.vstring(),
+    deleteNonConsumedUnscheduledModules = cms.untracked.bool(True),
+    dumpOptions = cms.untracked.bool(False),
+    emptyRunLumiMode = cms.obsolete.untracked.string,
+    eventSetup = cms.untracked.PSet(
+        forceNumberOfConcurrentIOVs = cms.untracked.PSet(
+            allowAnyLabel_=cms.required.untracked.uint32
+        ),
+        numberOfConcurrentIOVs = cms.untracked.uint32(0)
+    ),
+    fileMode = cms.untracked.string('FULLMERGE'),
+    forceEventSetupCacheClearOnNewRun = cms.untracked.bool(False),
+    holdsReferencesToDeleteEarly = cms.untracked.VPSet(),
+    makeTriggerResults = cms.obsolete.untracked.bool,
+    modulesToCallForTryToContinue = cms.untracked.vstring(),
+    modulesToIgnoreForDeleteEarly = cms.untracked.vstring(),
+    numberOfConcurrentLuminosityBlocks = cms.untracked.uint32(0),
+    numberOfConcurrentRuns = cms.untracked.uint32(1),
+    numberOfStreams = cms.untracked.uint32(0),
+    numberOfThreads = cms.untracked.uint32(1),
+    printDependencies = cms.untracked.bool(False),
+    sizeOfStackForThreadsInKB = cms.optional.untracked.uint32,
+    throwIfIllegalParameter = cms.untracked.bool(True),
+    wantSummary = cms.untracked.bool(False)
+)
+
+# Production Info
+process.configurationMetadata = cms.untracked.PSet(
+    annotation = cms.untracked.string('--conditions nevts:200'),
+    name = cms.untracked.string('Applications'),
+    version = cms.untracked.string('$Revision: 1.19 $')
+)
+
+# Output definition
+
+process.RECOoutput = cms.OutputModule("PoolOutputModule",
+    dataset = cms.untracked.PSet(
+        dataTier = cms.untracked.string('RECO'),
+        filterName = cms.untracked.string('')
+    ),
+    fileName = cms.untracked.string('file:hltOutput_RECO.root'),
+    outputCommands = process.RECOEventContent.outputCommands,
+    splitLevel = cms.untracked.int32(0)
+)
+
+# Additional output definition
+
+# Other statements
+from HLTrigger.Configuration.CustomConfigs import ProcessName
+process = ProcessName(process)
+
+from Configuration.AlCa.GlobalTag import GlobalTag
+process.GlobalTag = GlobalTag(process.GlobalTag, '140X_dataRun3_HLT_v3', '')
+
+# Path and EndPath definitions
+process.L1RePack_step = cms.Path(process.SimL1Emulator)
+process.endjob_step = cms.EndPath(process.endOfProcess)
+process.RECOoutput_step = cms.EndPath(process.RECOoutput)
+
+# Schedule definition
+# process.schedule imported from cff in HLTrigger.Configuration
+process.schedule.insert(0, process.L1RePack_step)
+process.schedule.extend([process.endjob_step,process.RECOoutput_step])
+from PhysicsTools.PatAlgos.tools.helpers import associatePatAlgosToolsTask
+associatePatAlgosToolsTask(process)
+
+
+
+# Customisation from command line
+
+# Add early deletion of temporary data products to reduce peak memory need
+from Configuration.StandardSequences.earlyDeleteSettings_cff import customiseEarlyDelete
+process = customiseEarlyDelete(process)
+# End adding early deletion
